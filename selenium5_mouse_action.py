e2593266b9594c1a93f270579c60db52a0dd3ca5
zhuwenqin <zhuwenqin_vendor@sensetime.com>
2020-03-30 13:24:21 +0800

    update

--- selenium5_mouse_action.py
+++ selenium5_mouse_action.py
@@ -6,16 +6,10 @@
 
 
 from selenium import webdriver
-
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
 """ 鼠标操作   
 常见的鼠标操作   详细了解  ActionChains 类"""
 from selenium.webdriver.common.action_chains import ActionChains
-
 import time
 
 driver = webdriver.Chrome()
---
+++ selenium6_select_option.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Time: 2020/3/30 12:51
+# @Author: wenqin_zhu
+# @File: selenium6_select_option.py
+# @Software: PyCharm
